0a4d5c44e5b80b9606ce422f91ee2c20f4f9b369
Markus Schneider <markus@icet-clan.de>
2016-11-16 17:00:41 +0100

    Added cmake files and structure

---
+++ include/sfb.h
@@ -0,0 +1,5 @@
+// Define macros for version numbers
+#define SFB_VERSION_MAJOR 0
+#define SFB_VERSION_MINOR 1
+#define SFB_VERSION_PATCH 0
+#define SFB_VERSION "0.1.0"
---
+++ src/sfb.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "sfb.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	printf("version %s\n", SFB_VERSION);
+	return 0;
+}
