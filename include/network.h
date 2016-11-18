288254d5d3c9b50d689628f4c64fbb8ee57cd8f4
Markus Schneider <markus@icet-clan.de>
2016-11-18 23:27:56 +0100

    Added network packets

--- include/network.h
+++ include/network.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include "sfb.h"
+#include "command.h"
 #include "serial/serial.h"
 
 using namespace std;
@@ -9,6 +10,18 @@ using namespace serial;
 
 class Network {
 private:
+	// Request packet structure
+	struct REQ_PACKET {
+		Command::COMMANDS	cmd;
+		string				param;
+	};
+
+	// Answer packet structure
+	struct ANS_PACKET {
+		Command::COMMANDS	cmd;
+		string				answer;
+	};
+
 	Serial ser;
 public:
 	Network();
